235f3c2eb0e2445278b6ad386e1a0f4403263b52
jovanniochoa <57188689+jovanniochoa@users.noreply.github.com>
2022-04-06 00:38:28 -0500

    Create prank.cpp
    virus prank

---
+++ prank.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int prank(){
+MessageBox(0,"Click OK to start DOWNLOADING virus.", "Minecraft", MB_OK );
+for (int i = 0; i <1000; i++){
+    cout << "Cache.exe" << endl;
+    cout << "Null. Release trojan virus " << endl;
+    cout << "Trojan Virus installing" << endl;
+    cout << "Updating Software..." << endl;
+    cout << "null_Trojan_Virus" << endl;
+    cout << "Uninstalling windows" << endl;
+    cout << "Gathering System file" << endl;
+    cout << "Windows32.exe" << endl;
+    cout << "NULL" << endl;
+    cout << "NULL. Increasing the speed of the sun to make a Windows file. NULL. Initializing." << endl;
+    cout << "Installing Cromium" << endl;
+    cout << "Initializing bank accout" << endl;
+    cout << "Gathering Bank account information" << endl;
+    cout << "Compiling key_logs" << endl;
+}
+    MessageBox(0, "Download Complete", "WINDOWS.exe", MB_OK);
+    for (int x = 0; x <10; x++){
+        MessageBox(0, "Download Complete", "WINDOWS.exe", MB_OK);
+    }
+}
